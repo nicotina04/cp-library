fbcb45713d85eb183be860126fecccb6d9162abd
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-04 01:58:18 +0900

    Update: Update segment tree lazy propagation

--- segment_tree/lazy_propagation.cpp
+++ segment_tree/lazy_propagation.cpp
@@ -14,7 +14,7 @@ class lazy_segtree {
 private:
   vector<T> lazy, tree;
 public:
-  lazy_segtree(int n): lazy(n * 4), tree(n * 4) { init(1, 1, n); }
+  lazy_segtree(int n): lazy(n * 4), tree(n * 4) {}
 
   T init(int node, int start, int end) {
     if (start == end) return tree[node] = ar[end];
@@ -22,20 +22,20 @@ public:
     return tree[node] = init(node * 2, start, m) + init(node * 2 + 1, m + 1, end);
   }
 
-  void propa(int node, int start, int end) {
+  void prop(int node, int start, int end) {
     if (!lazy[node]) return;
     tree[node] += (end - start + 1) * lazy[node];
-    if (start != end)
+    if (start ^ end)
       lazy[node * 2] += lazy[node], lazy[node * 2 + 1] += lazy[node];
     lazy[node] = 0;
   }
 
   void add(int node, int start, int end, int l, int r, T val) {
-    propa(node, start, end);
+    prop(node, start, end);
     if (start > r || end < l) return;
     if (l <= start && end <= r) {
       lazy[node] += val;
-      propa(node, start, end);
+      prop(node, start, end);
       return;
     }
     int m = mid(start, end);
@@ -45,7 +45,7 @@ public:
   }
 
   T sum(int node, int start, int end, int l, int r) {
-    propa(node, start, end);
+    prop(node, start, end);
     if (start > r || end < l) return 0;
     if (l <= start && end <= r) return tree[node];
     int m = mid(start, end);
