86db6c138579c2dfc20e26f133a55af3d5989888
HOGYEONG CHOE <devherdson@gmail.com>
2022-04-14 21:50:47 +0900

    Update: Update file name

--- utillity/rand_tc.cpp
+++ utillity/rand_tc.cpp
@@ -1,10 +1,10 @@
 #include "bits/stdc++.h"
 
 namespace rand_tc {
-  mt19937 generator(random_device());
-
   template <typename T>
   vector<T> get_rand_seq(int num, T low, T high) {
+    random_device rd;
+    mt19937 generator(rd());
     vector<T> ret(num);
     uniform_int_distribution<T> dt(low, high);
     for (auto& i : ret) i = dt(generator);
