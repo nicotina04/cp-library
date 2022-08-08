2c03c8e4790801eddc5590cf3acf99fe9a4a8a09
HOGYEONG CHOE <devherdson@gmail.com>
2022-08-09 01:06:38 +0900

    Update : Add static variables

--- string/trie.cpp
+++ string/trie.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 struct trie {
-  trie *child[26];
+  const static int sz = 26;
+  const static char offset = 'a';
+  trie *child[sz];
   bool terminal, haschild;
 
-  trie() : terminal(false), haschild(false) { fill(child, child + 26, nullptr); }
+  trie() : terminal(false), haschild(false) { fill(child, child + sz, nullptr); }
   ~trie() { for (auto &i : child) delete i; }
 
   void insert(const char *key) {
@@ -15,14 +17,14 @@ struct trie {
       return;
     }
     haschild = true;
-    int nxt = *key - 'A';
+    int nxt = *key - offset;
     if (child[nxt] == nullptr) child[nxt] = new trie();
     child[nxt]->insert(key + 1);
   }
 
   int find(const char *key) {
     if (*key == '\0') return terminal;
-    int nxt = *key - 'A';
+    int nxt = *key - offset;
     if (child[nxt] == nullptr) return 0;
     return child[nxt]->find(key + 1);
   }
