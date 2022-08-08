a0966151b8344a9b93b2cbf0132a1f155ad60605
HOGYEONG CHOE <devherdson@gmail.com>
2022-08-08 22:49:47 +0900

    Update : Fix typo

--- string/trie.cpp
+++ string/trie.cpp
@@ -7,9 +7,7 @@ struct trie {
   bool terminal, haschild;
 
   trie() : terminal(false), haschild(false) { fill(child, child + 26, nullptr); }
-  ~trie() {
-    for (int i = 0; i < 26; ++i) if (child[i] != nullptr) delete child[i];
-  }
+  ~trie() { for (auto &i : child) delete i; }
 
   void insert(const char *key) {
     if (*key == '\0') {
@@ -17,14 +15,14 @@ struct trie {
       return;
     }
     haschild = true;
-    int nxt = *key - 'a';
+    int nxt = *key - 'A';
     if (child[nxt] == nullptr) child[nxt] = new trie();
     child[nxt]->insert(key + 1);
   }
 
   int find(const char *key) {
     if (*key == '\0') return terminal;
-    int nxt = *key - 'a';
+    int nxt = *key - 'A';
     if (child[nxt] == nullptr) return 0;
     return child[nxt]->find(key + 1);
   }
