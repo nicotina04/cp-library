3625fac561c3633bd4c6d010c0713b514feb34f3
HOGYEONG CHOE <devherdson@gmail.com>
2022-08-29 23:09:46 +0900

    Add : Add count_divisors function O(N^(1/3))
    Add count_divisors working on O(N^(1/3))

---
+++ number_theory/count_divisors.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+#include "ext/rope"
+
+using namespace std;
+
+using ll = long long;
+
+vector<int> primes;
+bitset<1000001> nop;
+
+void sieve(int mx) {
+  for (int i = 2; i <= mx; i++) {
+    if (nop[i]) continue;
+    primes.push_back(i);
+    for (int j = i << 1; j <= mx; j += i) nop.set(j);
+  }
+}
+
+ll count_divisors(ll num) {
+  ll ret = 1;
+  for (ll i : primes) {
+    if (i * i * i > num) break;
+    int cnt = 1;
+    while (num % i == 0) num /= i, ++cnt;
+    ret *= cnt;
+  }
+  if (!nop[num]) ret <<= 1;
+  else {
+    ll lo = 1, hi = num;
+    bool good = false;
+    while (lo <= hi) {
+      auto mid = lo + (hi - lo) / 2;
+      if (mid * mid == num) {
+        good = true; // mid is prime
+        break;
+      }
+      if (mid * mid < num) lo = mid + 1;
+      else hi = mid - 1;
+    }
+    if (good) ret *= 3;
+    else ret *= 4;
+  }
+  return ret;
+}
