6674a30cb1a077354f44be14c3170a19291da630
HOGYEONG CHOE <devherdson@gmail.com>
2022-09-01 15:37:39 +0900

    Add : Add mo's algorithm structure

---
+++ segment_tree/mo.cpp
@@ -0,0 +1,16 @@
+int rt;
+
+struct query
+{
+  int l, r, order;
+  bool operator < (const query &arg) const
+  {
+    int x = r / rt, y = arg.r / rt;
+    return x != y ? l < arg.l : x < y;
+  }
+};
+
+void operation(int idx, bool yes)
+{
+  // Do something you want
+}
