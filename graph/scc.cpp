1e887958a5f22ffc931d164faee8f25d5e073772
HOGYEONG CHOE <devherdson@gmail.com>
2022-11-05 01:11:19 +0900

    Refactor: Change macro and const variable name
    Change macro and const variable name to MAXN

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-#define NODE_MAX 100001
-int discovered[NODE_MAX];
-bool finished[NODE_MAX];
-vector<int> al[NODE_MAX];
+constexpr int MAXN = 100001;
+int discovered[MAXN];
+bool finished[MAXN];
+vector<int> al[MAXN];
 stack<int> s;
 int cnt;
 vector<vector<int>> scc;
 int dfs(int cur) {
   int parent = discovered[cur] = cnt++;
-  s.push(cur);    
-  for (int i: al[cur]) {
-    if (discovered[i] == -1) parent = min(parent, dfs(i)); // CARE: init val == -1
+  s.push(cur);
+  for (int &i : al[cur]) {
+    if (discovered[i] == -1) parent = min(parent, dfs(i)); // CARE: init value is -1
     else if (!finished[i]) parent = min(parent, discovered[i]);
   }
   if (parent == discovered[cur]) {
     vector<int> group;
-    while (true) {
+    while (1) {
       int t = s.top();
       s.pop();
       group.push_back(t);
@@ -26,6 +26,6 @@ int dfs(int cur) {
       if (t == cur) break;
     }
     scc.push_back(group);
-  }  
+  }
   return parent;
 }
