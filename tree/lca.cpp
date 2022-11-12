91a447af555e11a966c73af3f37f95cde6a630c7
HOGYEONG CHOE <devherdson@gmail.com>
2022-11-12 13:06:58 +0900

    Refactor : Change macro to constexpr variable
    Change macro to constexpr variable

--- tree/lca.cpp
+++ tree/lca.cpp
@@ -2,13 +2,12 @@
 
 using namespace std;
 
-#define SPAS_LOG 22
-#define SPAS_NODE 1000001
-int spas[SPAS_LOG][SPAS_NODE];
-int h[SPAS_NODE];
+constexpr int MXLOG = 22, MXN = 1e6+10;
+int spas[MXLOG][MXN];
+int h[MXN];
 void do_spas() {
-  for (int i = 1; i < SPAS_LOG; ++i) {
-    for (int j = 1; j < SPAS_NODE; ++j) {
+  for (int i = 1; i < MXLOG; ++i) {
+    for (int j = 1; j < MXN; ++j) {
       spas[i][j] = spas[i - 1][spas[i - 1][j]];
     }
   }
@@ -16,10 +15,10 @@ void do_spas() {
 int lca(int u, int v) {
   if (h[u] < h[v]) swap(u, v);
   int diff = h[u] - h[v];
-  for (int i = SPAS_LOG - 1; i >= 0; --i)
+  for (int i = MXLOG - 1; i >= 0; i--)
     if (diff & (1 << i)) u = spas[u][i];
   if (u == v) return u;
-  for (int i = SPAS_LOG - 1; i >= 0; --i)
+  for (int i = MXLOG - 1; i >= 0; i--)
     if (spas[u][i] != spas[v][i])
       u = spas[u][i], v = spas[v][i];
   return spas[u][0];
